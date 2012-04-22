6cd3bd56ac5f638bb112c5b59bf75db43c260f5b
Sandro Boehme <sandro.boehme@gmx.de>
2012-04-22 16:40:42 +0200

    changed message to '...branch1'

--- testproject/src/main/java/testgroup/App.java
+++ testproject/src/main/java/testgroup/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello master branch!" );
+        System.out.println( "Hello branch1!" );
     }
 }
